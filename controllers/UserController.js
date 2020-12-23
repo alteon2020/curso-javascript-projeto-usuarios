0c76749003c50be80d0cc135fc6c1b74f2dadf7b
alteon2020 <alteonalves@gmail.com>
2020-12-22 22:00:50 -0300

    aula dia 22

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -5,6 +5,15 @@ class UserController {
     this.tableEl = document.getElementById(tableId);
 
     this.onSubmit();
+    this.onEdit();
+  }
+
+  onEdit() {
+    document.querySelector("#box-user-update .btn-cancel").addEventListener("click", e=> {
+
+        this.showPanelCreate();
+
+    }); 
   }
 
   onSubmit() {
@@ -14,41 +23,67 @@ class UserController {
 
       event.preventDefault();
 
+      let btn = this.formEl.querySelector("[type=submit]");
+
+      btn.disabled = true;
+
       let values = this.getValues();
 
-      this.getPhoto((content) =>{
+      if (!values) return false;
 
-        values.photo = content;
-        
-        this.addLine(values);
+      this.getPhoto().then(
 
-      });
+        (content) => {
+
+          values.photo = content;
+
+          this.addLine(values);
+
+          this.formEl.reset();
+
+          btn.disabled = false;
+
+        }, (e) => {
+          console.error(e);
+        }
+      );
 
     });
   }
 
-  getPhoto(callback) {
+  getPhoto() {
 
-    let fileReader = new FileReader();
+    return new Promise((resolve, reject) => {
 
-    let elements = [...this.formEl.elements].filter(item => {
+      let fileReader = new FileReader();
 
-      if (item.name === 'photo') {
+      let elements = [...this.formEl.elements].filter(item => {
 
-        return item;
+        if (item.name === 'photo') {
 
+          return item;
+
+        }
+      });
+
+      let file = elements[0].files[0];
+
+      fileReader.onload = () => {
+
+        resolve(fileReader.result);
+
+      };
+      fileReader.onerror = (e) => {
+        reject(e);
+      }
+      if (file) {
+        fileReader.readAsDataURL(file);
+      } else {
+        resolve('dist/img/boxed-bg.jpg');
       }
     });
 
-    let file = elements[0].files[0];
 
-    fileReader.onload = () => {
-
-      callback(fileReader.result);
-
-    };
-
-    fileReader.readAsDataURL(file);
   }
 
 
@@ -56,7 +91,17 @@ class UserController {
 
     let user = {};
 
+    let isValid = true;
+
     [...this.formEl.elements].forEach(function (field, index) {
+
+      if (['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value) {
+
+        field.parentElement.classList.add('has-error');
+
+        isValid = false;
+
+      }
 
       if (field.name == 'gender') {
 
@@ -64,12 +109,21 @@ class UserController {
 
           user[field.name] = field.value;
         }
+      } else if (field.name == 'admin') {
+        user[field.name] = field.checked;
+
       } else {
 
         user[field.name] = field.value;
       }
 
     });
+
+    if (!isValid) {
+
+      return false;
+    }
+
     return new User(
       user.name,
       user.gender,
@@ -84,17 +138,74 @@ class UserController {
 
   addLine(dataUser) {
 
-    this.tableEl.innerHTML = `
-    <tr>
+    let tr = document.createElement('tr');
+
+    tr.dataset.user = JSON.stringify(dataUser);
+
+    tr.innerHTML = `
       <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
       <td>${dataUser.name}</td>
       <td>${dataUser.email}</td>
-      <td>${dataUser.admin}</td>
-      <td>${dataUser.birth}</td>
+      <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
+      <td>${Utils.dateFormat(dataUser.register)}</td>
       <td>
-        <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+        <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
         <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-      </td>
-    </tr>`;
+      </td>`;
+
+    tr.querySelector('.btn-edit').addEventListener("click", e=>{
+      let json = JSON.parse(tr.dataset.user);
+       let form = document.querySelector("#form-user-update");
+      
+      for(let name in json){
+
+        let field = form.querySelector("[name="+ name.replace("_", "") + "]");
+  
+        if(field){
+          
+          if(field.type == 'file') continue;
+
+          field.value = json[name];
+
+        }
+
+      }
+
+      this.showPanelUpdate();
+
+    })
+    this.tableEl.appendChild(tr);
+    this.updateCount();
+  }
+
+  showPanelCreate(){
+
+    document.querySelector("#box-user-create").style.display = "block";
+    document.querySelector("#box-user-update").style.display = "none";
+
+  }
+
+  showPanelUpdate(){
+
+    document.querySelector("#box-user-create").style.display = "none";
+    document.querySelector("#box-user-update").style.display = "block";
+
+  }
+
+  updateCount() {
+    let numberUsers = 0;
+    let numberAdmin = 0;
+
+    [...this.tableEl.children].forEach(tr=>{
+
+      numberUsers++;
+
+      let user = JSON.parse(tr.dataset.user);
+
+      if(user._admin) numberAdmin++;
+    });
+
+    document.querySelector("#number-users").innerHTML = numberUsers;
+    document.querySelector("#number-users-admin").innerHTML = numberAdmin;
   }
 }
